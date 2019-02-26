e3f1a54b1c36bac717eb64b85d4773aec3753a41
David Acosta <davidacosta25081@yahoo.com>
2019-02-26 08:18:32 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,13 @@ require_relative './fancy_dance.rb'
 
 
 class Kid
+
+  attr_accessor :name
+   
+    def initialize(name)
+      @name = name
+    end
+
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
